2bfc39c51eb081587211dc2eb1bc26e7a2f79eef
rjrivera <rjrivera@mit.edu>
2015-05-19 20:01:25 -0400

    Updating database now clears email cache on server

--- mailBot.py
+++ mailBot.py
@@ -3,6 +3,8 @@ import sys
 
 cmdSSMTP = "mutt -s \"AFROTC Lead Update\" -a myTestDB.csv -- afrotc.det365@gmail.com < reportMail.txt"
 
+cmdRM = "rm -r new/*"
+
 p = subprocess.Popen(cmdSSMTP, shell = True, stderr=subprocess.PIPE)
 
 while True:
@@ -12,4 +14,16 @@ while True:
     if out != '':
         sys.stdout.write(out)
         sys.stdout.flush()
+
+p = subprocess.Popen(cmdRM, shell = True, stderr= subprocess.PIPE)
+
+while True:
+    out = p.stderr.read(1)
+    if out == '' and p.poll() != None:
+        break
+    if out != '':
+        sys.stdout.write(out)
+        sys.stdout.flush()
+
+
 print "*** Mailbot.py executed ***"
